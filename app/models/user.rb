6ffd20348c8ad3982532e5b1039eba05e2ae6c72
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-01 12:03:18 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  with_options presence: true do
+   validates :password, :password_confirmation, format: { with: /\A[a-zA-Z0-9]+\z/}
+   validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+   validates :family_name_kana, first_name_kana: { with:/\A[ァ-ヶー－]+\z/}
+  end
 end
