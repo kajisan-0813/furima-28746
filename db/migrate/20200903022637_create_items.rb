04d4c31a66331695d81e37747e64472746174866
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-03 11:49:41 +0900

    itemsテーブルの作成

---
+++ db/migrate/20200903022637_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name,                null:false
+      t.integer :price,              null:false
+      t.text :description,           null:false
+      t.integer :item_condition_id,  null:false
+      t.integer :category_id,        null:false
+      t.integer :shipping_cost_id,   null:false
+      t.integer :shipping_date_id,   null:false
+      t.integer :shipping_origin_id, null:false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_070025) do
+ActiveRecord::Schema.define(version: 2020_09_03_022637) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "description", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "category_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "shipping_date_id", null: false
+    t.integer "shipping_origin_id", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
