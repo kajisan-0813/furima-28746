d9a05dc86ab8157e9a721e75a4e38821ea71e7b6
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-04 17:40:12 +0900

    商品情報の詳細の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :item_condition
+
+  validates :category, :item_condition, presence: true
+  validates :category_id, :item_condition_id, numericality: { other_than: 1 } 
 end
--- db/migrate/20200903022637_create_items.rb
+++ db/migrate/20200903022637_create_items.rb
@@ -5,7 +5,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :price,              null:false
       t.text :description,           null:false
       t.integer :item_condition_id,  null:false
-      t.integer :category_id,        null:false
+      t.integer :category_id,      null:false
       t.integer :shipping_cost_id,   null:false
       t.integer :shipping_date_id,   null:false
       t.integer :shipping_origin_id, null:false
