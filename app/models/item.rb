d73fd11cc542359f60179b01ef5cb0f60a8c2ebd
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-04 19:12:17 +0900

    配送についての詳細の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :item_condition
+  belongs_to_active_hash :category, :item_condition, :shipping_cost, :shipping_origin, :shipping_date,
 
-  validates :category, :item_condition, presence: true
-  validates :category_id, :item_condition_id, numericality: { other_than: 1 } 
+  validates :category, :item_condition, :shipping_cost, :shipping_origin, :shipping_date, presence: true
+  validates :category_id, :item_condition_id, :shipping_cost_id, :shipping_origin_id, :shipping_date_id, numericality: { other_than: 1 } 
 end
