c5b57c3ed32696da613a103dffb9b8434dc6df85
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-10 22:00:41 +0900

    rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :items
-  has_many :transactions 
+  has_many :transactions
   with_options presence: true do
     validates :password, :password_confirmation, format: { with: /\A[a-zA-Z0-9]+\z/ }
     validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
