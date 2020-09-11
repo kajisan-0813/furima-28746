3d4cbb6f93b4d89887f2ca9dab56596b8e2ec8e5
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-11 21:20:27 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
   def index
     @items = Item.all.order('id DESC')
   end
@@ -26,7 +26,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     @item.update(item_params)
 
     if @item.update(item_params)
