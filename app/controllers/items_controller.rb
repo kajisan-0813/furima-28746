7de6a7dbdc9ea9119286e1e08777ed38897b9547
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-11 20:56:10 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
   def index
     @items = Item.all.order('id DESC')
   end
@@ -19,21 +20,32 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    if @item.save
+      redirect_to root_path
+    else
+      redirect_to edit_item_path
+    end
   end
+
+
+  
 
   private
 
   def item_params
     params.require(:item).permit(:name, :price, :item_condition_id, :description, :category_id, :shipping_cost_id, :shipping_date_id, :shipping_origin_id, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
