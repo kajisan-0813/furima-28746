b625536338f13d8435cfa7e9822bae5759aca5b9
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-13 18:05:35 +0900

    PAY.JPのGemを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  root to: "orders#index"
   resources :items, only: [:new, :create, :update, :index, :show, :edit] 
-  
+  resources :orders, only:[:create]
 end
