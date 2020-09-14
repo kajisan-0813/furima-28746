64b6a8a00d09262e38de1c45e45cd40a5f1bb259
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-14 10:48:35 +0900

    PAY.JPのGemの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,10 +4,19 @@ class OrdersController < ApplicationController
   end
   
   def new
+    @order = Destination.new
   end
 
   def create
+    @order = Destination.create(dastination_params)
   end
+
+  private
+
+  def orders_params
+    params.require(:destination).permit(:post_code,:prefecture,:city,:adress,:building_name,:phone_number,:item_transaction_id)
+  end
+
 
 end
 
