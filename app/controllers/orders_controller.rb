1e4b4f2dedb6742135c1d2593351f8fc8115bbe7
kajisan-0813 <yutakakajiwara0813@gmail.com>
2020-09-15 16:07:05 +0900

    商品購入機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @item =Item.find(params[:item_id])
   end
-  
-  def new
-    @order = Destination.new
-  end
 
+  def new
+    
+    @order = UserTransaction.new
+  end
+    
   def create
-    @order = Destination.create(dastination_params)
+    @item =Item.find(params[:item_id])
+    @order = UserTransaction.new(destination_params)
+    
+    if  @order.save  
+      redirect_to root_path
+    else
+      redirect_to item_orders_path   
+    end
   end
 
   private
 
-  def orders_params
-    params.require(:destination).permit(:post_code,:prefecture,:city,:adress,:building_name,:phone_number,:item_transaction_id).merge(user_id: current_user.id,item_id: params[:item_id])
+  def destination_params
+    params.permit(:post_code,:prefecture,:city,:adress,:building_name,:phone_number,:item_transaction_id).merge(user_id: current_user.id,item_id: params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_3b684ef7ee0b124d154af35f"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
+  end
+ 
 
   
 
---
+++ app/forms/user_transaction.rb
@@ -0,0 +1,16 @@
+class UserTransaction
+
+  include ActiveModel::Model
+  attr_accessor :post_code, :prefecture, :city, :adress, :building_name, :phone_number, :item_transaction_id,:user_id,:item_id
+
+  with_options presence: true do
+    validates :post_code, :prefecture, :city, :adress, :phone_number, :item_transaction_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+  end
+  validates :prefecture, numericality: { other_than: 0 }
+
+  def save
+    item_transaction = ItemTransaction.create(user_id: user_id, item_id: item_id)
+    Destination.create(post_code: post_code, prefecture: prefecture, city: city, adress: adress, building_name: building_name, phone_number: phone_number, item_transaction_id: item_transaction.id)
+  end
+end
